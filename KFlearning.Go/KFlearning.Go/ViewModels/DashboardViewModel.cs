880ec22b69c8e3cc1e44637c1fed65159e8804ad
fahminlb33 <fahminlb33@gmail.com>
2020-07-26 16:51:59 +0700

    Implement dashboard

--- KFlearning.Go/KFlearning.Go/ViewModels/DashboardViewModel.cs
+++ KFlearning.Go/KFlearning.Go/ViewModels/DashboardViewModel.cs
@@ -3,16 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace KFlearning.Go.ViewModels
 {
     public class DashboardViewModel : ViewModelBase
     {
-        public ICommand LoginCommand { get; set; }
+        public ICommand TestCommand { get; set; }
 
         public DashboardViewModel()
         {
-
+            TestCommand = new Command(async () =>
+            {
+                var browser = PageFactory.GetBrowserPage("https://google.com");
+                var shell = Application.Current.MainPage as MasterDetailPage;
+                await shell.Detail.Navigation.PushAsync(browser);
+            });
+            
         }
     }
 }
