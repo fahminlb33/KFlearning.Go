f0dca31218ebcff2852be9734eff35f8b4a902bd
fahminlb33 <fahminlb33@gmail.com>
2020-07-26 14:29:43 +0700

    Implemented porfile view

---
+++ KFlearning.Go/KFlearning.Go/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,19 @@
+﻿using KFlearning.Go.Infrastructure;
+using KFlearning.Go.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KFlearning.Go.ViewModels
+{
+    public class ProfileViewModel : ViewModelBase
+    {
+        private readonly UserService _userService = UserService.Instance;
+
+        public Uri ProfileUrl => _userService.Profile;
+
+        public string ProfileName => _userService.Name;
+
+        public string ProfileEmail => _userService.Email;
+    }
+}
--- KFlearning.Go/KFlearning.Go/Views/ProfileView.xaml.cs
+++ KFlearning.Go/KFlearning.Go/Views/ProfileView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KFlearning.Go.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace KFlearning.Go.Views
         public ProfileView()
         {
             InitializeComponent();
+            BindingContext = new ProfileViewModel();
         }
     }
 }
