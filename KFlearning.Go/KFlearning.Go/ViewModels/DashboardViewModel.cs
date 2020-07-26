86dcec8cbcbc49286b8a5eba202cbfc9b948deab
fahminlb33 <fahminlb33@gmail.com>
2020-07-26 17:06:47 +0700

    Add command to dashboard buttons

--- KFlearning.Go/KFlearning.Go/ViewModels/DashboardViewModel.cs
+++ KFlearning.Go/KFlearning.Go/ViewModels/DashboardViewModel.cs
@@ -1,25 +1,52 @@
 ﻿using KFlearning.Go.Infrastructure;
+using KFlearning.Go.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace KFlearning.Go.ViewModels
 {
     public class DashboardViewModel : ViewModelBase
     {
-        public ICommand TestCommand { get; set; }
+        private readonly UserService _userService = UserService.Instance;
+
+        public ICommand OpenHomepageCommand { get; set; }
+        public ICommand SendFeedbackCommand { get; set; }
 
         public DashboardViewModel()
         {
-            TestCommand = new Command(async () =>
+            OpenHomepageCommand = new Command(OpenHomepage_Handler);
+            SendFeedbackCommand = new Command(SendFeedback_Handler);
+        }
+
+        private async void OpenHomepage_Handler()
+        {
+            await Browser.OpenAsync("https://kodesiana.com");
+        }
+
+        private async void SendFeedback_Handler()
+        {
+            try
             {
-                var browser = PageFactory.GetBrowserPage("https://google.com");
-                var shell = Application.Current.MainPage as MasterDetailPage;
-                await shell.Detail.Navigation.PushAsync(browser);
-            });
-            
+                var message = new EmailMessage
+                {
+                    Subject = "KFlearning Go Support Feedback",
+                    Body = "Halo Admin! Saya pengguna KFlearning Go, ada saran yang ingin saya sampaikan!\n\n--Tulis saran Anda disini--",
+                    To = new List<string>(){ "support@kodesiana.com" }
+                };
+                await Email.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException fbsEx)
+            {
+                await Application.Current.MainPage.DisplayAlert("KFlearning Go", "Perangkat Anda tidak mendukung email.", "Tutup");
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("KFlearning Go", "Gagal membuat email", "Tutup");
+            }
         }
     }
 }
